e55664773190a9c14d1c1f36bd193b7e11dad659
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2023-01-13 14:12:45 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,34 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root to: 'public/homes#top'
+get "home/about" => "public/homes#about", as: "about"
+
+scope module: :public do
+    delete '/cart_items/all' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :items, only: [:index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    resources :orders, only: [:new, :create, :index, :show]
+    get '/customers/my_page' => 'customers#show'
+    get '/customers/information/edit' => 'customers#edit'
+    patch '/customers/update' => 'customers#update'
+    get '/customers/unsubscribe' => 'customers#unsubscribe', as: :unsubscribe
+    patch '/customers/withdraw' => 'customers#withdraw', as: :withdrawal
+    resources :postals, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  get 'items/new'
+  post 'items' => 'items#create'
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :genres, only: [:create, :index, :edit, :update]
+    resources :items, only: [:index, :create, :new, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_125025) do
+ActiveRecord::Schema.define(version: 2023_01_13_064021) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2023_01_12_125025) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,6 +40,26 @@ ActiveRecord::Schema.define(version: 2023_01_12_125025) do
     t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "postals", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
