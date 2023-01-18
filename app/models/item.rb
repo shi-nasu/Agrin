d40a197a0ecbdcecb3eb29dadac4103c588afd9c
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2023-01-18 12:07:26 +0000

    update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  has_many :order_details
+  has_many :cart_items
+  belongs_to :genre
+  has_one_attached :image
+
+  def get_profile_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
--- db/migrate/20230113063906_create_items.rb
+++ db/migrate/20230113063906_create_items.rb
@@ -2,6 +2,12 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
 
+      t.integer :genre_id
+      t.text :introduction
+      t.string :name
+      t.integer :price
+      t.boolean :is_active, default: true
+      
       t.timestamps
     end
   end
